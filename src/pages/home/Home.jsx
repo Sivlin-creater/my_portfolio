abdbc89892cd7c6da3ec5c7fdce7874ec7e1f64c
Sivlin-creater <sivlinchhay01@gmail.com>
2026-02-08 21:58:27 +0700

    Moved some code and updated image

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Profile from '../../assets/wildanimal.jpg'
+import Profile from '../../assets/4_6image.jpg'
 import { Link } from 'react-router'
 import { RiArrowLeftSLine } from 'react-icons/ri'
 import './home.css'
